d06910ff40edd9f0168e034eb34f078d7564ea32
olia112 <geraskina.olia2010@yandex.ru>
2014-09-25 16:39:01 +0400

    Update Igra.java

--- src/Igra.java
+++ src/Igra.java
@@ -12,8 +12,8 @@ public class Igra {
             System.out.println("У вас 7 попыток");
             for (int i = 1; i < 8; i++){
                 System.out.println(i+ " попытка. Необходимо ввести число");
-                int a = in.nextInt();
-                if (number == a){
+                int guess = in.nextInt();
+                if (number == guess){
                     System.out.println("Поздравляем!!! Вы угадали!!!");
                     break;
                 }
