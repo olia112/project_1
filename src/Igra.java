85d90a18aa4fadbadce70037d1b3a203512d6f81
olia112 <geraskina.olia2010@yandex.ru>
2014-09-25 16:13:23 +0400

    Create Igra.java

---
+++ src/Igra.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Оля on 25.09.2014.
+ */
+public class Igra {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        Scanner in = new Scanner (System.in);
+        int number = rnd.nextInt(100);
+            System.out.println("У вас 7 попыток");
+            for (int i = 1; i < 8; i++){
+                System.out.println(i+ " попытка. Необходимо ввести число");
+                int a = in.nextInt();
+                if (number == a){
+                    System.out.println("Поздравляем!!! Вы угадали!!!");
+                    break;
+                }
+                else {
+                    System.out.println("Вы не угадали. Попробуйте ещё раз. У вас осталось "+ (7-i) +"попыток");
+                }
+                if (i == 7){
+                    System.out.println("Было загадано число "+ number);
+                }
+            }
+    }
+}
