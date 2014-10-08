9f137354b1158c53a707cf25f188ba8bc26491c1
olia112 <geraskina.olia2010@yandex.ru>
2014-10-08 23:01:02 +0400

    Update Igra.java

--- src/Igra.java
+++ src/Igra.java
@@ -10,19 +10,18 @@ public class Igra {
         Scanner in = new Scanner (System.in);
         int number = rnd.nextInt(100);
             System.out.println("У вас 7 попыток");
-            for (int i = 1; i < 8; i++){
-                System.out.println(i+ " попытка. Необходимо ввести число");
-                int guess = in.nextInt();
-                if (number == guess){
-                    System.out.println("Поздравляем!!! Вы угадали!!!");
-                    break;
+                for (int i = 1; i < 8; i++){
+                    System.out.println(i+ " попытка. Необходимо ввести число");
+                    int guess = in.nextInt();
+                    if (number == guess){
+                        System.out.println("Поздравляем!!! Вы угадали!!!");
+                        break;
+                    } else {
+                        System.out.println("Вы не угадали. Попробуйте ещё раз. У вас осталось "+ (7-i) +"попыток");
+                    }
+                    if (i == 7){
+                        System.out.println("Было загадано число "+ number);
+                    }
                 }
-                else {
-                    System.out.println("Вы не угадали. Попробуйте ещё раз. У вас осталось "+ (7-i) +"попыток");
-                }
-                if (i == 7){
-                    System.out.println("Было загадано число "+ number);
-                }
-            }
     }
 }
